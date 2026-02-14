9d090410bf7eb612246d5385b8d47ffa65282d8c
jripke74 <jripke74@gmail.com>
2026-02-14 09:20:16 -0800

    add db info

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-// require('dotenv').config();
+require('dotenv').config();
 
 const express = require("express");
-// const { Pool } = require("pg");
+const { Pool } = require("pg");
 const path = require("path");
 const bodyParser = require("body-parser");
 
 const app = express();
-// const port = 3000;
+const port = 3000;
 
 // Middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname))); // server static files (like index.html)
 
 // PostgreSQl connection pool configuration
-// const pool = new Pool({
-//   user: process.env.DB_USERNAME,         // e.g., postres
-//   host: 'localhost',
-//   database: process.env.DB_NAME,        // e.g., userdb
-//   password: process.env.DB_PASSWORD, // your postgres password
-//   port: process.env.DB_PORT,                // default postres port
-// });
+const pool = new Pool({
+  user: process.env.DB_USERNAME,         // e.g., postres
+  host: 'localhost',
+  database: process.env.DB_NAME,        // e.g., userdb
+  password: process.env.DB_PASSWORD, // your postgres password
+  port: process.env.DB_PORT,                // default postres port
+});
 
 // Test database connection on start up
-// pool.connect()
-//   .then(() => console.log('Connected to the PostgresSQL database'))
-//   .catch(err => console.error('Failed to connect to the database', err.stack));
+pool.connect()
+  .then(() => console.log('Connected to the PostgresSQL database'))
+  .catch(err => console.error('Failed to connect to the database', err.stack));
 
 // Route to serve the HTML page
 app.get('/', (req, res) => {
@@ -32,20 +32,24 @@ app.get('/', (req, res) => {
 });
 
 // Route to handle form submission
-// app.post('/submit-form', async (req, res) => {
-//   const { title, releaseyear } = req.body;
-//   try {
-//     const query = 'INSERT INTO movies(titel, releaseyear) VALUES($1, $2) RETURNING *';
-//     const values = [ title, releaseyear ];
-//     const result = await pool.query(query, values);
-//     console.log('Data inserted:', result.rows[0]);
-//     res.status(200).send('Data submitted successfully!');
-//   } catch (err) {
-//     console.error('Error inserting data:', err.stack);
-//     res.status(500).send('Error submitting data');
-//   }
+app.post('/submit-form', async (req, res) => {
+  const { title, releaseyear } = req.body;
+  try {
+    const query = 'INSERT INTO movies(titel, releaseyear) VALUES($1, $2) RETURNING *';
+    const values = [ title, releaseyear ];
+    const result = await pool.query(query, values);
+    console.log('Data inserted:', result.rows[0]);
+    res.status(200).send('Data submitted successfully!');
+  } catch (err) {
+    console.error('Error inserting data:', err.stack);
+    res.status(500).send('Error submitting data');
+  }
+});
+
+// app.listen(3000, () => {
+//   console.log('Server running on port 3000');
 // });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:${port}');
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });
